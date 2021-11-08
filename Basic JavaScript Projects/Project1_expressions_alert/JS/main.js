e6f360d8cce366a1d9a32d49c12290e98c6c388d
jccstgo <jccstgo@gmail.com>
2021-11-08 11:01:52 -0800

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,14 +2,31 @@
 document.write("Hello World!");
 
 
-
+//This is an example about use fontcolor
 var A = " This is a string";
+var A = A.fontcolor("blue");
 document.write(A);
 
 /*
-var B="This is the Assignment Step 49"
+var B="This is the Assignment Step 49";
+var B = B.fontcolor("blue");
 window.alert(B);
 */
 
+//This is an example about concatenated
+document.write("\"<br> This an example of backslash\"  and\\or"+ " <br> \"Example don\'t\" <br>");
 
-document.write("\"<br> This an example of backslash\"  and\\or <br> \"Example don\'t\"");
+//This is an example about print variables
+var C = "This string is"+"concatenated <br>";
+var C = C.fontcolor("green");
+document.write(C);
+
+//This is an example about variables
+var a="a",b="b",c="c";
+var b = b.fontcolor("red");
+document.write(b+"<br>");
+
+//This is an example about expresions
+document.write(2+3+"<br>");
+
+
