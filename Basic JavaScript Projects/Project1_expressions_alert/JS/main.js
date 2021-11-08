995d172f8dd1e4ac56a80e1c685816c0433602a5
jccstgo <jccstgo@gmail.com>
2021-11-08 10:43:45 -0800

    Strings

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,15 @@
 //window.alert("Hello, World!");
 document.write("Hello World!");
+
+
+
+var A = " This is a string";
+document.write(A);
+
+/*
+var B="This is the Assignment Step 49"
+window.alert(B);
+*/
+
+
+document.write("\"<br> This an example of backslash\"  and\\or <br> \"Example don\'t\"");
